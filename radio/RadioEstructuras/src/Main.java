81b9461ebf7f0609f7a57aeb2cbfddc7d4537601
robertriosm <87707371+robertriosm@users.noreply.github.com>
2022-01-22 15:54:10 -0600

    modificaciones y pruebas

--- radio/RadioEstructuras/src/Main.java
+++ radio/RadioEstructuras/src/Main.java
@@ -1,5 +1,16 @@
+
+import org.junit.*;
+
 public class Main {
     public static void main(String[] args) {
-        MyFrame frame = new MyFrame();
+        MyFrame frame = new MyFrame(new RadioG8());
+    }
+
+    @Test
+    public void test1() {
+        RadioG8 radio1 = new RadioG8();
+        radio1.nextStation(true);
+        assertEquals(540, radio1.getStation());
+
     }
 }
